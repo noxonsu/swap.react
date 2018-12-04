fab4ac9496714a74f1678b4fad5427524d85e2a8
Pasha8914 <uhau-buhau@mail.ru>
2018-12-04 19:52:08 +1000

    ститль кода

--- shared/components/modals/DownloadModal/DownloadModal.js
+++ shared/components/modals/DownloadModal/DownloadModal.js
@@ -55,9 +55,9 @@ export default class DownloadModal extends React.Component {
     return (
       <Modal name={name} title="We don`t store your private keys and will not be able to restore them!">
         <div styleName="subTitle">
-          <p><FormattedMessage id="down57" defaultMessage="It seems like you're using an IPhone or an IPad." /></p>
-          <p><FormattedMessage id="down58" defaultMessage="Just copy this keys and paste into notepad textarea" /> </p>
-          <p><FormattedMessage id="down59" defaultMessage="Or make the screen shot" /> </p>
+          <p1><FormattedMessage id="down57" defaultMessage="It seems like you're using an IPhone or an IPad." /></p1>
+          <p1><FormattedMessage id="down58" defaultMessage="Just copy this keys and paste into notepad textarea" /> </p1>
+          <p1><FormattedMessage id="down59" defaultMessage="Or make the screen shot" /> </p1>
         </div>
         <CopyToClipboard text={text} onCopy={this.handleCopyText}>
           <Button styleName="button" brand disabled={isTextCopied}>
@@ -67,21 +67,31 @@ export default class DownloadModal extends React.Component {
             }
           </Button>
         </CopyToClipboard>
-        <div styleName="style">
-          <div><FormattedMessage id="down70" defaultMessage="Ethereum address: " /><p>{ethData.address}</p></div>
-          <div><FormattedMessage id="down71" defaultMessage="Ethereum Private key: " /><p>{ethData.privateKey}</p></div>
+        <div styleName="content">
+          <p1><FormattedMessage id="down70" defaultMessage="Ethereum address: " /></p1>
+          <p>{ethData.address}</p>
+          <p1><FormattedMessage id="down71" defaultMessage="Ethereum Private key: " /></p1>
+          <p>{ethData.privateKey}</p>
 
-          <div><FormattedMessage id="down73" defaultMessage="Bitcoin address: " /><p>{btcData.address}</p></div>
-          <div><FormattedMessage id="down74" defaultMessage="Bitcoin Private key: " /><p>{btcData.privateKey}</p></div>
+          <p1><FormattedMessage id="down73" defaultMessage="Bitcoin address: " /></p1>
+          <p>{btcData.address}</p>
+          <p1><FormattedMessage id="down74" defaultMessage="Bitcoin Private key: " /></p1>
+          <p>{btcData.privateKey}</p>
 
-          <div><FormattedMessage id="down76" defaultMessage="EOS Master Private Key: " /><p>{eosData.masterPrivateKey}</p></div>
-          <div><FormattedMessage id="down77" defaultMessage="EOS Account name: " /><p>{eosData.address}</p></div>
+          <p1><FormattedMessage id="down76" defaultMessage="EOS Master Private Key: " /></p1>
+          <p>{eosData.masterPrivateKey}</p>
+          <p1><FormattedMessage id="down77" defaultMessage="EOS Account name: " /></p1>
+          <p>{eosData.address}</p>
 
-          <div><FormattedMessage id="down79" defaultMessage="TELOS Active Private Key: " /><p>{telosData.activePrivateKey}</p></div>
-          <div><FormattedMessage id="down80" defaultMessage="TELOS Account name: " /><p>{telosData.address}</p></div>
+          <p1><FormattedMessage id="down79" defaultMessage="TELOS Active Private Key: " /></p1>
+          <p>{telosData.activePrivateKey}</p>
+          <p1><FormattedMessage id="down80" defaultMessage="TELOS Account name: " /></p1>
+          <p>{telosData.address}</p>
 
-          <div><FormattedMessage id="down81" defaultMessage="Litecoin address: " /><p>{ltcData.address}</p></div>
-          <div><FormattedMessage id="down83" defaultMessage="Litecoin Private key: " /><p>{ltcData.privateKey}</p></div>
+          <p1><FormattedMessage id="down81" defaultMessage="Litecoin address: " /></p1>
+          <p>{ltcData.address}</p>
+          <p1><FormattedMessage id="down83" defaultMessage="Litecoin Private key: " /></p1>
+          <p>{ltcData.privateKey}</p>
         </div>
       </Modal>
     )
